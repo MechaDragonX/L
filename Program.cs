ebf1016294576a063556671b39d619e3799a4f26
MechaDragonX <theofficialrv@gmail.com>
2020-01-07 16:22:34 -0800

    Extract text from word and return a List<string>

--- Program.cs
+++ Program.cs
@@ -1,4 +1,12 @@
 ﻿using System;
+using System.Text;
+using System.Xml;
+using DocumentFormat.OpenXml;
+using System.IO;
+using System.IO.Packaging;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using System.Collections.Generic;
 
 namespace L
 {
@@ -6,7 +14,31 @@ namespace L
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string workingDir = Environment.CurrentDirectory;
+            string projectDir = Directory.GetParent(workingDir).Parent.Parent.FullName;
+
+            printList(ExtractFromWord(projectDir + "/data/rv.docx"));
+        }
+        private static List<string> ExtractFromWord(string path)
+        {
+            List<string> list = new List<string>();
+            using (WordprocessingDocument doc = WordprocessingDocument.Open(path, false))
+            {
+                Body body = doc.MainDocumentPart.Document.Body;
+                foreach(OpenXmlElement element in body.ChildElements)
+                {
+                    list.Add(element.InnerText);
+                }
+            }
+            return list;
+        }
+
+        private static void printList<T>(List<T> list)
+        {
+            foreach(T item in list)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
