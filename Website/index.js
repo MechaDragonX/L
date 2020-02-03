a99e331d2360ada191df09cd760659259152908d
MechaDragonX <theofficialrv@gmail.com>
2020-02-02 22:29:38 -0800

    Check file type when uploading and reject non text documents

--- Website/index.js
+++ Website/index.js
@@ -3,41 +3,31 @@ const {S3} = require('aws-sdk');
 const fileUpload = require('express-fileupload');
 const exphbs = require('express-handlebars');
 const app = express();
-app.use(express.static('views'));
-app.use(express.static('styles'));
+const {lookup} = require('mime-types');
+const {checkMimeType, uploadFile} = require('./util');
 const config = require('./config.json');
-
 const s3 = new S3({
     accessKeyId: config.accessKey,
     secretAccessKey: config.secretAccessKey
 });
 
-const uploadFile = (file, cb) => {
-    const params = {
-        Bucket: config.bucketName,
-        Key: file.name,
-        Body: Buffer.from(file.data, 'binary')
-    };
-    s3.upload(params, cb);
-};
-
-app.engine('hbs', exphbs({extname:'hbs'}));
+app.use(express.static('views'));
+app.use(express.static('styles'));
+app.use(fileUpload());
+app.engine('hbs', exphbs({ extname:'hbs' }));
 app.set('view engine', 'hbs');
-app.use(fileUpload());  
 
 app.get('/', (req, res) => {
-    return res.render('index')
+    return res.render('index', { title: 'Resume Upload Tool' });
 });
 
 app.post('/', (req, res) => {
-    uploadFile(req.files.resume, (err, data) => {
-        if(err == null) {
-            return res.render('success', { data });
-        }
-        else {
-            console.log(err);
-            return res.render('error');
-        }
+    if(!checkMimeType(lookup(req.files.resume.name)))
+        return res.render('index', { title: 'Resume Upload Tool', error: true, message: 'Unsupported File Type!' });
+    uploadFile(s3, req.files.resume, (err, data) => {
+        if(err)
+            return res.render('index', { title: 'Resume Upload Tool', error: true, message: 'Something went Wrong!' });;
+        return res.render('success', { title: 'Resume Upload Tool', data });
     });
 });
 
