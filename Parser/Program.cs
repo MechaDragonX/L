fc297f6ba860b463751aee8b6b27fcb6fc6bd97e
MechaDragonX <theofficialrv@gmail.com>
2020-02-03 13:41:25 -0800

    Remove uncessarry lines in Parser

--- Parser/Program.cs
+++ Parser/Program.cs
@@ -15,31 +15,13 @@ namespace L
 
 			// string projectDir = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
 			string[] lines = FileParser.ExtractAllLines(path);
-			// PrintArray(lines);
-
-			// Applicant name = Applicant.Deserialize("filename"));
 
 			ResumeParser resumeParser = new ResumeParser(lines);
 			Applicant applicant = resumeParser.Parse();
-			// Console.WriteLine(applicant);
 			applicant.Serialize(true);
 			Console.WriteLine("\nAll done!! ^_^\nThe Applicant's data was written to the file \"<surname>, <given name>.json\" in a \"out\" folder in the provided folder!");
 			Console.WriteLine("Thanks for using the program!");
 			Console.ReadLine();
-		}
-		private static void PrintArray<T>(T[] array)
-		{
-			foreach (T item in array)
-			{
-				Console.WriteLine(item);
-			}
-		}
-		private static void PrintList<T>(List<T> list)
-		{
-			foreach(T item in list)
-			{
-				Console.WriteLine(item);
-			}
 		}
 	}
 }
